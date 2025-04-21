2cb0ddabeb83f4c8d53ddf49547d626ce26031eb
Grégory Paul <me@gpaul.me>
2025-04-21 14:25:08 +0200

    chore: remove vite-plugin-pwa and use local serviceworker

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgrPlugin from 'vite-plugin-svgr'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(() => {
     return {
         base: './',
-        plugins: [
-            react(),
-            svgrPlugin(),
-            VitePWA({ registerType: 'autoUpdate' }),
-        ],
+        plugins: [react(), svgrPlugin()],
     }
 })
