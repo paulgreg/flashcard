82d83794fed1ea464c75ff18b515f82adac31f03
Grégory Paul <me@gpaul.me>
2025-04-21 11:36:11 +0200

    feat: add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgrPlugin from 'vite-plugin-svgr'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(() => {
     return {
         base: './',
-        plugins: [react(), svgrPlugin()],
+        plugins: [
+            react(),
+            svgrPlugin(),
+            VitePWA({ registerType: 'autoUpdate' }),
+        ],
     }
 })
